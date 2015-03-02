73cc4037bbe8a01e37aae71dee6b832173040566
Richard Tanner <dicktanner@gmail.com>
2015-03-01 16:57:15 -0800

    Mailer setup

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,9 +1,42 @@
 'use strict';
 angular.module('portfolioApp')
-  .controller('ContactCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
+ $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData; //formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+    $scope.submit = function(contactform) {
+        $scope.submitted = true;
+        $scope.submitButtonDisabled = true;
+        if (contactform.$valid) {
+          $scope.data = {
+            from: $scope.formData.inputEmail,
+            to: 'richardCzechowski@gmail.com',
+            subject: $scope.formData.inputSubject,
+            text: $scope.formData.inputMessage
+          };
+          $http({
+            method  : 'POST',
+            url     : '../mailer/contact-form.php',
+            data    : $scope.data,
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+          }).success(function(data){
+            console.log(data);
+            if (data.success) { //success comes from the return json object
+              $scope.submitButtonDisabled = true;
+              $scope.resultMessage = data.message;
+              $scope.result='bg-success';
+            } else {
+              $scope.submitButtonDisabled = false;
+              $scope.resultMessage = data.message;
+              $scope.result='bg-danger';
+            }
+          });
+        } else {
+          $scope.submitButtonDisabled = false;
+          $scope.resultMessage = 'Failed <img src="http://www.chaosm.net/blog/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley">  Please fill out all the fields.';
+          $scope.result='bg-danger';
+        }
+    }
   }]);
