ade6458ad49d024c3bf30bd5be6899d659c0e368
Richard Tanner <dicktanner@gmail.com>
2015-03-05 15:08:03 -0800

    Create custom paper element, add search, skills, cards.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,11 @@
 'use strict';
 angular.module('portfolioApp')
 .controller('MainCtrl',['$scope', '$timeout', 'SettingsFactory', function ($scope, $timeout, SettingsFactory) {
+  //Variable Initialization
   var portfolio = new Firebase("https://portfolio-web.firebaseio.com/");
   $scope.data = { projects: [], temp: [] };
+  $scope.techs = {};
+  //End Variable Initialization
 
   $scope.getData = function (){
     portfolio.on("value", function(snapshot) {
@@ -13,8 +16,9 @@ angular.module('portfolioApp')
         }
         $timeout(function() {
           $scope.data.projects = $scope.data.temp;
+          $scope.sortSkills($scope.data.projects);
           $scope.toggles = SettingsFactory.get();
-          console.log($scope.toggles);
+          console.log($scope.data.projects);
         })
       });
     });
@@ -27,4 +31,17 @@ angular.module('portfolioApp')
     return date;
   };
 
+  $scope.sortSkills = function(projects){
+    for (var i = 0; i< projects.length; i++){
+      for (var k = 0; k<projects[i].technology.length; k++){
+        if ($scope.techs.hasOwnProperty(projects[i].technology[k])){
+          $scope.techs[projects[i].technology[k]].count++;
+        }
+        else{
+          $scope.techs[projects[i].technology[k]] = {value: true, count: 1};
+        }
+      }
+    }
+    console.log($scope.techs);
+  }
 }]);
